f3fcc53f69ef42b0dc8679ca15c492a6879fdabd
Ígor Chagas <igorchagas@denox.com.br>
2016-04-14 08:02:59 -0300

    Arquivo inicial.

---
+++ rem.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct WORD
+{
+    char *letters;
+    int length;
+    int line;
+}WORD;
+
+int isStringEmpty(char *string, int length){
+    int flag = 1;
+    if(length > 0){
+        int i;
+        char c;
+        for(i = 0; i < length; i++){
+            if(string[i] != ' ')
+                flag = 0;
+        }
+    }
+    return flag;
+}
+
+struct WORD createWord(char *letters, int length, int line){
+    struct WORD w;
+    w.letters = letters;
+    w.length = length;
+    w.line = line;
+    return w;
+}
+
+void printWord(struct WORD w){
+    int i = 0;
+    for(i = 0; i < w.length; i++){
+        printf("%c", w.letters[i]);
+    }
+    for(i = (25 - w.length); i > 0; i--){
+        printf(" ");
+    }
+    printf("%d\n", w.line);
+}
+
+void readWordFromFile(FILE *fp){
+    if(fp == NULL){
+        printf("%s\n", "Fail to open file!");
+    }
+    else{
+        char chars[101];
+        char c;
+        int line = 1;
+        int i = 0;
+        while ((c = fgetc(fp)) != EOF) {
+            if(c != ' ' && c != '\n'){
+                chars[i] = c;
+            }
+            else{
+                if(!isStringEmpty(chars, i)){
+                    printWord(createWord(chars, i, line));
+                }
+                if(c == '\n')
+                    line++;
+                i = -1;
+            }
+            i++;
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    FILE *fp;
+    char path[] = "kant.txt";
+    fp = fopen(path, "r+");
+    readWordFromFile(fp);
+    fclose(fp);
+    return 0;
+}
